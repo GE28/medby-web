e9d3c6db258707e7a8e01e5d9fc764755babb6c5
GE28 <44209041+GE28@users.noreply.github.com>
2021-05-06 14:55:31 -0300

    Save user data in localStorage after login

--- src/global/UserContext.tsx
+++ src/global/UserContext.tsx
@@ -1,13 +1,23 @@
 import React, { FC, createContext, useState, useCallback } from 'react';
 
-import api from '../services/axios';
+import axios from '../services/axios';
+
+type UserData = Record<string, unknown> & {
+  name: string;
+};
 
 interface LoginParams {
   email: string;
   password: string;
 }
 
+interface UserState {
+  data: UserData;
+  token: string;
+}
+
 interface UserContextBody {
+  user: UserState;
   login(data: LoginParams): void;
 }
 
@@ -16,15 +26,29 @@ export const userContext = createContext({} as UserContextBody);
 const { Provider } = userContext;
 
 export const UserProvider: FC = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    const token = localStorage.getItem('@medby/user_token');
+    const data = localStorage.getItem('@medby/user_data');
+
+    return (token && data
+      ? { token, data: JSON.parse(data) }
+      : {}) as UserState;
+  });
+
   const login = useCallback(async ({ email, password }) => {
     try {
-      const response = await api.post('login', { email, password });
+      const response = await axios.post('login', { email, password });
 
-      console.log(response.data);
+      const { user: data, token } = response.data;
+
+      localStorage.setItem('@medby/user_token', token);
+      localStorage.setItem('@medby/user_data', JSON.stringify(data));
+
+      setUser({ token, data });
     } catch (err) {
-      console.log({ err });
+      console.log(err.response.data);
     }
   }, []);
 
-  return <Provider value={{ login }}>{children}</Provider>;
+  return <Provider value={{ user, login }}>{children}</Provider>;
 };
