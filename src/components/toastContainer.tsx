53aed5d99e1906a85279ed90e11e95ac9e65f3be
GE28 <44209041+GE28@users.noreply.github.com>
2021-05-13 21:52:41 -0300

    Add responsive way to show toasts

--- src/components/toastContainer.tsx
+++ src/components/toastContainer.tsx
@@ -1,66 +1,19 @@
 import React, { FC, useContext } from 'react';
 
-import { toastContext, ToastData } from '../global/ToastContext';
+import { toastContext } from '../global/ToastContext';
 
 import Toast from './toast';
 
 import { ToastsContainer as StyledToastsContainer } from './styles';
 
-type ToastWithoutID = Omit<ToastData, 'id'>;
-
-const tempToast: ToastWithoutID = {
-  title: 'Teste no sistema',
-  message: 'Acalme-se, é somente um teste',
-};
-
-const tempErrorToast: ToastWithoutID = {
-  type: 'error',
-  title: 'Erro no sistema',
-  message: 'Um pouco de desespero cairia bem :O',
-};
-
-const tempSucessToast: ToastWithoutID = {
-  type: 'success',
-  title: 'Sucesso no sistema',
-  message: 'Bora celebrar!',
-};
-
 const ToastsContainer: FC = () => {
-  const { toastDataList, addToast } = useContext(toastContext);
+  const { toastDataList } = useContext(toastContext);
 
   const toastContainerContent = toastDataList.map((toast) => (
     <Toast {...toast} key={toast.id} />
   ));
 
-  return (
-    <StyledToastsContainer>
-      {toastContainerContent}
-      <button
-        type="button"
-        onClick={() => {
-          addToast(tempToast);
-        }}
-      >
-        CLICK ME
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          addToast(tempErrorToast);
-        }}
-      >
-        NO CLICK ME
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          addToast(tempSucessToast);
-        }}
-      >
-        PFFFF CLIMME INSTEAD
-      </button>
-    </StyledToastsContainer>
-  );
+  return <StyledToastsContainer>{toastContainerContent}</StyledToastsContainer>;
 };
 
 export default ToastsContainer;
