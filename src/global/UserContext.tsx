0cdcd98410ea1d15437e395920c59aea2f5598e0
GE28 <44209041+GE28@users.noreply.github.com>
2021-05-13 21:55:05 -0300

    Add authentication functions

--- src/global/UserContext.tsx
+++ src/global/UserContext.tsx
@@ -1,4 +1,20 @@
-import React, { FC, createContext, useState, useCallback } from 'react';
+/* eslint-disable import/no-duplicates */
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import React, {
+  FC,
+  createContext,
+  useState,
+  useCallback,
+  useContext,
+} from 'react';
+
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+import decodeJWT, { JwtPayload } from 'jwt-decode';
+
+import { toastContext } from './ToastContext';
 
 import axios from '../services/axios';
 
@@ -11,17 +27,27 @@ interface LoginParams {
   password: string;
 }
 
+interface RegisterParams {
+  name: string;
+  email: string;
+  cpf: string;
+  password: string;
+}
+
 interface UserState {
   data: UserData;
   token: string;
 }
 
-interface UserContextBody {
+interface UserContext {
   user: UserState;
   login(data: LoginParams): void;
+  logout(): void;
+  register(data: RegisterParams): void;
+  isTokenValid(): boolean;
 }
 
-export const userContext = createContext({} as UserContextBody);
+export const userContext = createContext({} as UserContext);
 
 const { Provider } = userContext;
 
@@ -35,9 +61,53 @@ export const UserProvider: FC = ({ children }) => {
       : {}) as UserState;
   });
 
-  const login = useCallback(async ({ email, password }) => {
+  const { addToast } = useContext(toastContext);
+
+  const login = useCallback(async (loginParams: LoginParams) => {
+    const { email, password } = loginParams;
+
     try {
       const response = await axios.post('login', { email, password });
+      const { user: data, token } = response.data;
+
+      localStorage.setItem('@medby/user_token', token);
+      localStorage.setItem('@medby/user_data', JSON.stringify(data));
+
+      setUser({ token, data });
+
+      addToast({
+        type: 'success',
+        title: `Bem-vindo novamente, ${data.name.split(' ', 1)}`,
+        message: format(new Date(), "'Hoje é dia 'P', são 'p' ('z')'", {
+          locale: ptBR,
+        }),
+      });
+    } catch (err) {
+      addToast({
+        type: 'error',
+        title: 'Falha no login',
+        message: 'Confira os dados inseridos e tente novamente',
+      });
+    }
+  }, []);
+
+  const logout = useCallback(() => {
+    localStorage.removeItem('@medby/user_token');
+    localStorage.removeItem('@medby/user_data');
+
+    setUser({} as UserState);
+  }, []);
+
+  const register = useCallback(async (registerParams: RegisterParams) => {
+    const { name, email, cpf, password } = registerParams;
+
+    try {
+      const response = await axios.post('register', {
+        name,
+        email,
+        cpf,
+        password,
+      });
 
       const { user: data, token } = response.data;
 
@@ -45,10 +115,45 @@ export const UserProvider: FC = ({ children }) => {
       localStorage.setItem('@medby/user_data', JSON.stringify(data));
 
       setUser({ token, data });
+
+      addToast({
+        type: 'success',
+        title: `Bem-vindo novamente, ${data.name.split(' ', 1)}`,
+        message: format(new Date(), "'Hoje é dia 'P', são 'p' ('z')'", {
+          locale: ptBR,
+        }),
+      });
     } catch (err) {
-      console.log(err.response.data);
+      addToast({
+        type: 'error',
+        title: 'Falha ao realizar registro',
+        message: 'Confira os dados inseridos e tente novamente',
+      });
     }
   }, []);
 
-  return <Provider value={{ user, login }}>{children}</Provider>;
+  const isTokenValid = useCallback(() => {
+    if (!user.token) {
+      return false;
+    }
+
+    try {
+      const { exp }: JwtPayload = decodeJWT(user.token);
+
+      if (exp && Date.now() >= exp * 1000) {
+        return false;
+      }
+    } catch (err) {
+      logout();
+      return false;
+    }
+
+    return true;
+  }, [user]);
+
+  return (
+    <Provider value={{ user, login, logout, register, isTokenValid }}>
+      {children}
+    </Provider>
+  );
 };
