7e7e3d6e2faf21f98c5ba511450388a8c67301f0
GE28 <44209041+GE28@users.noreply.github.com>
2021-05-24 16:40:26 -0300

    Fix authentication-based routes

--- src/routes/authRoutes.tsx
+++ src/routes/authRoutes.tsx
@@ -1,47 +1,49 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { ComponentType, FC, useContext } from 'react';
+import React, { ComponentType, FC, useContext, useEffect } from 'react';
 import { Redirect, Route, RouteProps } from 'react-router-dom';
 
 import { userContext } from '../global/UserContext';
 
 interface AuthRoute extends RouteProps {
   component: ComponentType<any>;
+  guest?: boolean;
 }
 
 export const GuestRoute: FC<AuthRoute> = ({
+  guest = true,
   component: Component,
   ...rest
 }) => {
   const { user } = useContext(userContext);
 
+  const pathname = guest ? '/home' : '/';
+
   return (
     <Route
       {...rest}
       render={(props) => {
-        return user.data ? (
-          <Redirect to={{ pathname: '/home' }} />
-        ) : (
-          <Component {...props} />
-        );
+        const [redirect, component] = [
+          <Redirect to={{ pathname }} />,
+          <Component {...props} />,
+        ];
+
+        if (!guest) {
+          return user.data ? component : redirect;
+        }
+
+        return user.data ? redirect : component;
       }}
     />
   );
 };
 
 export const UserRoute: FC<AuthRoute> = ({ component: Component, ...rest }) => {
-  const { isTokenValid } = useContext(userContext);
+  const { tokenValidator } = useContext(userContext);
 
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        return isTokenValid() ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to={{ pathname: '/' }} />
-        );
-      }}
-    />
-  );
+  useEffect(() => {
+    tokenValidator();
+  }, []);
+
+  return <GuestRoute {...rest} component={Component} guest={false} />;
 };
