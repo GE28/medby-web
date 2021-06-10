effe93b256acc865ba510b13b2749240314ae259
GE28 <44209041+GE28@users.noreply.github.com>
2021-06-10 14:07:34 -0300

    Finish account page

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,7 +1,42 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+
+import { userContext } from '../../global/UserContext';
+
+import AvatarForm from './avatarForm';
+import EmailForm from './emailForm';
+import PasswordForm from './passwordForm';
+
+import GitHubLink from '../../components/githubLink';
+import Header from '../../components/header';
+import Footer from '../../components/footer';
+import Profile from '../../components/profile';
+
+import { Container } from '../styles';
+import { MainContent } from './styles';
 
 const AccountPage: FC = () => {
-  return <h1>Mudar dados da conta</h1>;
+  const { user } = useContext(userContext);
+
+  return (
+    <Container>
+      <MainContent>
+        <Header logged>
+          <Profile
+            avatarLink={user.data.avatar}
+            name={user.data.name.split(' ')[0]}
+          />
+        </Header>
+
+        <AvatarForm />
+        <EmailForm />
+        <PasswordForm />
+      </MainContent>
+
+      <Footer>
+        <GitHubLink />
+      </Footer>
+    </Container>
+  );
 };
 
 export default AccountPage;
