137e239c59cb00bf3689749c0929410eb7dc68e1
GE28 <44209041+GE28@users.noreply.github.com>
2021-06-25 19:50:34 -0300

    Convert Form component from styled to native

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,29 +1,6 @@
-import styled from 'styled-components/macro';
+import React, { FC } from 'react';
+import { Form as StyledForm } from './styles';
 
-const Form = styled.form`
-  width: 436px;
-  display: flex;
-  flex-direction: column;
-  row-gap: 15px;
-
-  @media (max-width: 576px) {
-    max-width: calc(100% - 84px);
-  }
-
-  button {
-    margin-top: 24px;
-  }
-
-  a {
-    color: #54428e;
-    font-size: 14px;
-    font-weight: 600;
-  }
-
-  p {
-    margin-top: -8px;
-    text-align: center;
-  }
-`;
+const Form: FC = ({ children }) => <StyledForm>{children}</StyledForm>;
 
 export default Form;
--- src/components/styles.ts
+++ src/components/styles.ts
@@ -324,6 +324,34 @@ export const Footer = styled.footer`
   padding: 12px;
 `;
 
+export const Form = styled.form`
+  width: 436px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+
+  @media (max-width: 576px) {
+    max-width: calc(100% - 84px);
+  }
+
+  button {
+    margin-top: 24px;
+  }
+
+  a {
+    color: #54428e;
+    font-size: 14px;
+    font-weight: 600;
+  }
+
+  p {
+    margin-top: -8px;
+    text-align: center;
+  }
+`;
+
+export default Form;
+
 export const GithubContainer = styled.a`
   display: flex;
   align-items: center;
