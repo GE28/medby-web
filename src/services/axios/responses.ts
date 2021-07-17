9520c7481b1ebc5ed99b595658cd675a3c312e04
GE28 <44209041+GE28@users.noreply.github.com>
2021-07-17 16:18:25 -0300

    Add responses

--- src/services/axios/responses.ts
+++ src/services/axios/responses.ts
@@ -34,6 +34,40 @@ export type AppointmentsDataResponse = [
   },
 ];
 
+// get /appointments
+export type UnitsDataResponse = [
+  Record<string, string> & {
+    id: string;
+    name: string;
+    cep: string;
+    complements: string;
+  },
+];
+
+export type SpecsDataResponse = [
+  Record<string, string> & {
+    id: string;
+    name: string;
+    display_name: string;
+    base_price: string;
+  },
+];
+
+type AvailableTime = Record<string, string> & {
+  _id: string;
+  doctor_id: string;
+  date: string;
+  spec_id: string;
+  unit_id: string;
+};
+
+export type AvailableTimesDataResponse = {
+  data: AvailableTime[];
+  config: {
+    maxAllowedDaysInFuture: number;
+  };
+};
+
 // get https://viacep.com.br/ws/20740010/json/
 export type ViaCep = Record<string, string> & {
   cep: string;
