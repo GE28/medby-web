064a8ab103dc08c8562fbcc0ab03fc3c6a2c2083
GE28 <44209041+GE28@users.noreply.github.com>
2021-08-04 14:08:34 -0300

    Add AppointmentPreviewResponse type

--- src/services/axios/responses.ts
+++ src/services/axios/responses.ts
@@ -6,33 +6,33 @@ export interface UserDataResponse extends Record<string, string | null> {
   avatar: string | null;
 }
 
-// get /appointments
-export type AppointmentsDataResponse = [
-  Record<string, string> & {
-    id: string;
-    time: string;
-    final_price: string;
+export type AppointmentData = {
+  id: string;
+  time: string;
+  final_price: string;
 
-    doctor_id: string;
-    doctor: {
-      avatar: string;
-      document: string;
-      name: string;
+  doctor_id: string;
+  doctor: {
+    avatar: string | null;
+    document: string;
+    name: string;
 
-      spec_id: string;
-      specialty: {
-        display_name: string;
-      };
-
-      unit_id: string;
-      unit: {
-        name: string;
-        cep: string;
-        complements: string;
-      };
+    spec_id: string;
+    specialty: {
+      display_name: string;
     };
-  },
-];
+
+    unit_id: string;
+    unit: {
+      name: string;
+      cep: string;
+      complements: string;
+    };
+  };
+};
+
+// get /appointments
+export type AppointmentsDataResponse = AppointmentData[];
 
 // get /appointments
 export type UnitsDataResponse = [
@@ -44,6 +44,30 @@ export type UnitsDataResponse = [
   },
 ];
 
+// get /appointments/available
+export type AvailableTime = Record<string, string> & {
+  _id: string;
+  doctor_id: string;
+  date: string;
+  spec_id: string;
+  unit_id: string;
+};
+
+// get /appointments/available?config_only
+export type AvailableTimesDataResponse = {
+  data: AvailableTime[];
+  config: {
+    maxAllowedDaysInFuture: number;
+  };
+};
+
+// get /appointments/available/<id>
+export type AppointmentPreviewResponse = Omit<AppointmentData, 'id'> & {
+  aT_id: string;
+  aT_date: string;
+};
+
+// get /specialties
 export type SpecsDataResponse = [
   Record<string, string> & {
     id: string;
@@ -53,23 +77,8 @@ export type SpecsDataResponse = [
   },
 ];
 
-export type AvailableTime = Record<string, string> & {
-  _id: string;
-  doctor_id: string;
-  date: string;
-  spec_id: string;
-  unit_id: string;
-};
-
-export type AvailableTimesDataResponse = {
-  data: AvailableTime[];
-  config: {
-    maxAllowedDaysInFuture: number;
-  };
-};
-
 // get https://viacep.com.br/ws/20740010/json/
-export type ViaCep = Record<string, string> & {
+export type ViaCep = {
   cep: string;
   logradouro: string;
   complemento: string;
